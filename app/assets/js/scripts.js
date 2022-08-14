dd40a6950d9326abbe0b44125552309adc5e4517
Syoma Iwauchi <syoma4645@gmail.com>
2022-08-14 11:43:34 +0900

    [add] first-commit

---
+++ app/assets/js/scripts.js
@@ -0,0 +1,65 @@
+/*
+ * ====================================================================
+ * Grow Template
+ * @package  Grow Template
+ * @author   GrowGroup.Inc <info@grow-group.jp>
+ * @license  MIT Licence
+ * ====================================================================
+ */
+
+(function ($) {
+  var growApp = function () {
+
+  };
+
+  /************************
+   * please insert your code
+   *************************/
+  growApp.prototype.myCode = function () {
+  }
+
+  growApp.prototype.enterAnimation = function () {
+    let $loader = $('.c-loader');
+    if (!$loader.length === 0) {
+      return false;
+    }
+
+    function changeClass(el, className, time) {
+      return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+          el.addClass(className);
+          resolve(className);
+        }, time);
+      });
+    }
+
+    var loaderFunction = function () {
+      // ここから実行
+      if ($loader) {
+        changeClass($loader, "is-active", 1000).then(function () {
+          changeClass($loader, "is-close", 2000).then(function () {
+            changeClass($loader, "is-hidden", 1000).then(function () {
+
+            });
+          });
+        });
+      }
+    };
+
+    // first entry only
+    if (!sessionStorage.getItem('loading')) {
+      sessionStorage.setItem('loading', true);
+      loaderFunction();
+    } else {
+      // テストはこちらをコメントアウト外す
+      // loaderFunction();
+      $loader.addClass('is-already');
+    }
+  };
+
+  $(function () {
+    var app = new growApp();
+    app.myCode();
+    app.enterAnimation();
+  });
+})(jQuery);
